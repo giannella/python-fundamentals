7ccfa9808f4b3b4cf9d6d5d55ba533333a45e836
Eric Giannella <giannella@berkeley.edu>
2014-08-20 09:54:00 -0700

    hw

--- challenges/02-Strings/A_defining_strings.py
+++ challenges/02-Strings/A_defining_strings.py
@@ -4,15 +4,16 @@
 ## You can see that you can create problems when you use a quote character in the
 ## string. There are a couple ways around this, including putting a backslash
 ## before the problematic quote, or using a different quote (")
-print 'This person's name will be defined very well!'
+print "This person's name will be defined very well!"
 
 # Define your name
 ## Define strings called your_first_name and your_last_name (they can be
 ## anything)
 
+your_first_name = "Eric "
+your_last_name = "Giannella"
 
-
-print your_first_name, your_last_name, 'was here!'
+print your_first_name+ your_last_name+" was here!"
 
 # Your strings are sequences (of characters)
 ## Use subscripting to pull out the second character of your last name.
@@ -27,4 +28,6 @@ print your_first_name, your_last_name, 'was here!'
 ## So, if s = 'HelpA', s[3] is 'p'. Don't forget that we start with 0 (no
 ## distance into the string).
 
-print 'The second letter of my last name is' # ???
+ln = 'giannella'
+
+print 'The second letter of my last name is '+ln[-2]
