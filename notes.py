b9b222c039b710b476506f00ec13f146b266e697
Eric Giannella <giannella@berkeley.edu>
2014-08-20 13:51:48 -0700

    stuff done during class

--- notes.py
+++ notes.py
@@ -2,3 +2,13 @@ __author__ = 'Eric Giannella'
 
 #random stuff
 
+firstName = "Johan"
+print(firstName.center(20, '*'))
+print('Johan Gambolputty'.split())
+
+print(str.lower(firstName))
+
+print(firstName.count("h"))
+
+print(firstName.lstrip("J")+" removed first letter using lstrip")
+
